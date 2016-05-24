8604a2f71f9a598164e138fbee023ce1e5a8ddbb
eayin2 <>
2016-05-24 22:10:36 +0200

    fixed colors

--- helputils/core.py
+++ helputils/core.py
@@ -10,7 +10,19 @@ import traceback
 from difflib import SequenceMatcher
 from subprocess import Popen, PIPE
 
-red, yellow, normal, green = "\x1b[31m", "\x1b[33m", "\x1b[30m", "\x1b[32m"
+
+def _wrap_with(code):
+
+    def inner(text, bold=False):
+        c = code
+        if bold:
+            c = "1;%s" % c
+        return "\033[%sm%s\033[0m" % (c, text)
+    return inner
+
+
+red, green, yellow = _wrap_with('31'), _wrap_with('32'), _wrap_with('33')
+blue, magenta, cyan, blue = _wrap_with('34'), _wrap_with('35'), _wrap_with('36'), _wrap_with('37')
 
 
 class SetupLogger():
@@ -40,15 +52,16 @@ class SetupLogger():
 
     def error(self, message):
         func = inspect.currentframe().f_back.f_code
-        self.logger.error("{0}(E) {1} {2}{3}".format(red, func.co_name, message, normal))
+        self.logger.error("{0}(E) {1}".format(func.co_name, red(message)))
 
     def warning(self, message):
         func = inspect.currentframe().f_back.f_code
-        self.logger.warning("{0}(W) {1} {2}{3}".format(yellow, func.co_name, message, normal))
+        self.logger.warning("{0}(W) {1}".format(func.co_name, (message)))
+        self.logger.warning("{0}".format(cyan(message)))
 
     def debug(self, message):
         func = inspect.currentframe().f_back.f_code
-        self.logger.debug("{0}(D) {1} {2}{3}".format(green, func.co_name, message, normal))
+        self.logger.debug("{0}(D) {1}".format(func.co_name, magenta(message)))
 
     def newline(self, how_many_lines=1):
         for i in range(how_many_lines):
@@ -56,6 +69,21 @@ class SetupLogger():
 
 
 log = SetupLogger('vvv')
+
+
+def isremotefile(f, hn=False):
+    """Checks if file is a file, either locally or remotely."""
+    cmd = ["file", "-b", f]
+    if hn:
+        cmd = ["ssh", hn] + cmd
+    p1 = Popen(cmd, stdout=PIPE)
+    out = p1.communicate()[0].decode("UTF-8")
+    if "No such file" in out:
+        return None
+    elif "directory" in out:
+        return False
+    else:
+        return True
 
 
 def islocal(hn1):
@@ -89,16 +117,13 @@ def is_number(s):
         return False
 
 
-def find_mount_point(path, hn=False):
-    """Finds mount point for remote (requires that the name of the ssh alias is the hostname's name) and local system
-       recursively by traversing directories in reverse direction."""
-    if hn and not islocal(hn):
-        p1 = Popen([ssh, hn, "findmnt", "-T", path, "-n", "-o", "TARGET"], stdout=PIPE)
-        path = p1.communicate[0].decode("UTF-8")
-    else:
-        path = os.path.abspath(path)
-        while not os.path.ismount(path):
-            path = os.path.dirname(path)
+def find_mountpoint(path, hn=False):
+    """Returns mountpoint from local or given remote system (requires ssh alias for hostname to be configured)."""
+    cmd = ["findmnt", "-T", path, "-n", "-o", "TARGET"]
+    if hn:
+        cmd = ["ssh", hn] + cmd
+    p1 = Popen(cmd, stdout=PIPE)
+    path = p1.communicate()[0].decode("UTF-8").strip()
     log.info("mountpoint is: %s" % path)
     return path
 
